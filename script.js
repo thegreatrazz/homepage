9a82e19b93bf1974df70dd80e7c84c3325d7bce0
Raresh Nistor <me@thegreatrazz.nz>
2020-10-11 15:55:13 +1300

    Update bg, add bangs back in, and remove uni links

--- script.js
+++ script.js
@@ -1,74 +1,77 @@
 
 const bangs = [
     // Google stuff
-    [ '!g', 'Google' ],
-    [ '!gphotos', 'Google Photos' ],
-    [ '!maps', 'Google Maps' ],
+    ['!g', 'Google'],
+    ['!gphotos', 'Google Photos'],
+    ['!maps', 'Google Maps'],
 
-    //
-    [ '!img', 'Search images' ],
-    [ '!video', 'Search videos' ],
-    
+    // additional searches
+    ['!img', 'Search images'],
+    ['!video', 'Search videos'],
+
+    // tools
+    ['!bitly', 'Shorten links'],
+    ['!isitup', 'Is the website up?'],
+    ['!isitup', 'Is the website up?'],
+
     // Media consumption
-    [ '!yt', 'YouTube' ],
-    [ '!spy', 'Spotify' ],
-    [ '!scloud', 'Soundcloud' ],
-    [ '!xkcd', 'sudo make me a sandwitch' ],
-    [ '!xkcd', 'xkcd' ],
-    [ '!netflix', 'Netflix' ],
-    [ '!netflix', 'and chill' ],
-    [ '!tweet', 'Twitter' ],
-    [ '!r', 'Reddit' ],
+    ['!yt', 'YouTube'],
+    ['!spy', 'Spotify'],
+    ['!applemusic', 'Apple Music'],
+    ['!scloud', 'Soundcloud'],
+    ['!xkcd', 'sudo make me a sandwitch'],
+    ['!xkcd', 'xkcd'],
+    ['!netflix', 'Netflix'],
+    ['!netflix', 'and chill'],
+    ['!tweet', 'Twitter'],
+    ['!r', 'Reddit'],
 
     // UNIX
-    [ '!die', 'Linux Manual Pages' ],
-    [ '!tldr', 'Manual page was <i>too long; didn\'t read</i>' ],
-    [ '!dman', 'Debian Manual Pages' ],
-    [ '!dpkg', 'Debian packages' ],
-    [ '!dbugs', 'Debian bugs' ],
-    [ '!debfiles', 'Which pkg does the file belong to?' ],
-    [ '!pacman', 'Arch Linux packages' ],
-    [ '!aur', 'Arch User Repositories' ],
-    [ '!archwiki', 'Arch Linux Wiki' ],
-    [ '!time', '' ]
-    
+    ['!die', 'Linux Manual Pages'],
+    ['!tldr', 'Manual page was too long; didn\'t read'],
+    ['!dman', 'Debian Manual Pages'],
+    ['!dpkg', 'Debian packages'],
+    ['!dbugs', 'Debian bugs'],
+    ['!debfiles', 'Which pkg does the file belong to?'],
+    ['!pacman', 'Arch Linux packages'],
+    ['!aur', 'Arch User Repositories'],
+    ['!archwiki', 'Arch Linux Wiki'],
+    ['!time', 'UNIX timestamp']
+
     // Programming
-    [ '!sx', 'Stack Exchange' ],
-    [ '!npm', 'Node Package Manager' ],
-    
+    ['!sx', 'Stack Exchange'],
+    ['!npm', 'Node Package Manager'],
+
     // Maths
-    [ '!calc', 'Calculator' ],
-    [ '!wa', 'Wolfram Alpha' ],
-    [ '!wa', 'Calculator powered by magic' ],
+    ['!calc', 'Calculator'],
+    ['!wa', 'Wolfram Alpha'],
+    ['!wa', 'Calculator powered by magic'],
 
     // Translation
-    [ '!tr', 'Translate' ],
-    [ '!gtro', 'Translate to Romanian' ],
-    [ '!gtro', 'traduce către limba română' ],
-    [ '!gtja', 'Translate to Japanese' ],
-    [ '!gtja', '日本語にホンヤクする' ],
-    [ '!gtja', '<code>ALL YOUR BASE ARE BELONG TO US</code>' ],
+    ['!tr', 'Translate'],
+    ['!gtro', 'Translate to Romanian'],
+    ['!gtro', 'traduce către limba română'],
+    ['!gtja', 'Translate to Japanese'],
+    ['!gtja', '日本語にホンヤクする'],
+    ['!gtja', 'ＡＬＬ　ＹＯＵＲ　ＢＡＳＥ　ＡＲＥ　ＢＥＬＯＮＧ　ＴＯ　ＵＳ'],
 
     // Dictionary
-    [ '!jisho', '辞書&mdash;Japanese Dictionary' ],
-    [ '!dexonline', 'Dicționarul explicativ a limbei române' ]
+    ['!jisho', '辞書 Japanese Dictionary'],
+    ['!dexonline', 'Dicționarul explicativ a limbei române']
 
     // Reading
-    [ '!med', 'Medium' ],
+    ['!med', 'Medium'],
 ]
 
 $(() => {
     $("#search input").focus(() => {
-        var index = Math.floor(Math.random() * bangs.length)
-        var bang = bangs[index]
+        var index = Math.floor(Math.random() * bangs.length);
+        var bang = bangs[index];
 
-        $("#bang-suggestion").html('<b>' + bang[0] + '</b> ' + bang[1])
-        $("#bangs").slideDown(150)
-        // $("#sections").slideUp(250, 'linear')
+        $("#search input").attr("placeholder", bang[0] + ' — ' + bang[1]);
     })
 
     $("#search input").blur(() => {
-        $("#bangs").delay(225).slideUp(150)
-        // $("#sections").slideDown(250, 'linear')
+        $("#search input").attr("placeholder", "Search the internet");
     })
 })
